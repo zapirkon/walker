b1bb528779d63fea497e207d0166052f7bc93711
zapirkon <zapirkon@gmail.com>
2022-07-17 08:44:57 +0200

    Completed crossroads

--- src/crossroad.test.ts
+++ src/crossroad.test.ts
@@ -1,68 +1,214 @@
 import { Crossroad, Direction } from "./crossroad";
-import { Step } from "./step";
+import { Step, StepType } from "./step";
 
 describe('Crossroads', () => {
-  test('valid crossroad', () => {
-    expect(true).toBe(true);
-  });
 
-  test('not a crossroad', () => {
-    expect(false).toBe(false);
-  });
+  const makeChoices = (up: string, down: string, left: string, right: string)
+    : Record<Direction, Step> => ({
+      UP: new Step(up),
+      DOWN: new Step(down),
+      LEFT: new Step(left),
+      RIGHT: new Step(right),
+    });
 
-  describe('crossing the road', () => {
+  describe('valid crossroads', () => {
+    it('start must have one choice for any start direction', () => {
+      const crossroad = new Crossroad(
+        new Step(StepType.START),
+        Direction.RIGHT,
+        makeChoices('x', '', '', '')
+      );
+      expect(crossroad.move()).toStrictEqual(Direction.UP);
+    });
+
     it('continue in same direction', () => {
-      const step = new Step('-');
-      const direction = Direction.RIGHT;
-      const expected = new Step('-');
       const crossroad = new Crossroad(
-        step,
-        direction,
-        {
-          UP: new Step(''),
-          DOWN: new Step(''),
-          LEFT: new Step(''),
-          RIGHT: expected,
-        }
+        new Step('-'),
+        Direction.RIGHT,
+        makeChoices('', '', '', '-')
       );
-      const move = crossroad.move();
-      expect(move).toStrictEqual(expected);
+      expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
     });
 
-    it('turns', () => {
-      const step = new Step('+');
-      const direction = Direction.RIGHT;
-      const expected = new Step('D');
-      const crossroad = new Crossroad(
-        step,
-        direction,
-        {
-          UP: new Step(''),
-          DOWN: expected,
-          LEFT: new Step(''),
-          RIGHT: new Step('-'),
-        }
-      );
-      const move = crossroad.move();
-      expect(move).toStrictEqual(expected);
+    describe('simple turn', () => {
+      it('up', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.LEFT,
+          makeChoices('U', '', '', '-')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.UP);
+      });
+
+      it('down', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.RIGHT,
+          makeChoices('', 'D', '', '-')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.DOWN);
+      });
+
+      it('left', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.UP,
+          makeChoices('|', '|', 'L', '')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.LEFT);
+      });
+
+      it('right', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.DOWN,
+          makeChoices('|', '|', '', 'R')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+    });
+
+    describe('cross bridges', () => {
+      it('up', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.LEFT,
+          makeChoices('-', '', '', '-')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.UP);
+      });
+
+      it('down', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.RIGHT,
+          makeChoices('', '-', '', '-')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.DOWN);
+      });
+
+      it('left', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.UP,
+          makeChoices('|', '|', '|', '')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.LEFT);
+      });
+
+      it('right', () => {
+        const crossroad = new Crossroad(
+          new Step('+'),
+          Direction.DOWN,
+          makeChoices('|', '|', '', '|')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+    });
+
+    describe('letter choice', () => {
+      it('continue to path', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices(' ', ' ', ' ', '-')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+
+      it('continue to bridge', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices(' ', ' ', ' ', '|')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+
+      it('continue to letter', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices(' ', ' ', ' ', 'R')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+
+      it('continue when it can turn', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices('U', 'D', 'L', 'R')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.RIGHT);
+      });
+
+      it('turn', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices('U', ' ', 'L', '')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.UP);
+      });
+
+      it('turn to bridge', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices('-', ' ', 'L', '')
+        );
+        expect(crossroad.move()).toStrictEqual(Direction.UP);
+      });
+
+      it('stop on fork', () => {
+        const crossroad = new Crossroad(
+          new Step('A'),
+          Direction.RIGHT,
+          makeChoices('U', 'D', 'L', '')
+        );
+        expect(crossroad.move()).toStrictEqual(undefined);
+      });
     });
   });
 
-  test('fork', () => {
-    const step = new Step('+');
-    const direction = Direction.RIGHT;
-    const crossroad = new Crossroad(
-      step,
-      direction,
-      {
-        UP: new Step('|'),
-        DOWN: new Step('|'),
-        LEFT: new Step(''),
-        RIGHT: new Step('-'),
-      }
-    );
-    const move = crossroad.move();
-    expect(move).toStrictEqual(undefined);
+  describe('dead end', () => {
+
+    it('stuck at start', () => {
+      const crossroad = new Crossroad(
+        new Step(StepType.START),
+        Direction.RIGHT,
+        makeChoices('U', 'D', 'L', 'R')
+      );
+      expect(crossroad.move()).toStrictEqual(undefined);
+    });
+
+    it('end was reached', () => {
+      const crossroad = new Crossroad(
+        new Step(StepType.END),
+        Direction.RIGHT,
+        makeChoices('', '', '@', 'R')
+      );
+      expect(crossroad.move()).toStrictEqual(undefined);
+    });
+
+    test('nothing ahead', () => {
+      const crossroad = new Crossroad(
+        new Step('N'),
+        Direction.RIGHT,
+        makeChoices('', '', '', '')
+      );
+      expect(crossroad.move()).toStrictEqual(undefined);
+    });
+
+    test('fork', () => {
+      const crossroad = new Crossroad(
+        new Step('+'),
+        Direction.RIGHT,
+        makeChoices('|', '|', '-', '-')
+      );
+      expect(crossroad.move()).toStrictEqual(undefined);
+    });
   });
 
 });
--- src/crossroad.ts
+++ src/crossroad.ts
@@ -6,17 +6,20 @@ export const Direction = {
   LEFT: 'LEFT',
   RIGHT: 'RIGHT',
 } as const;
-type Direction = typeof Direction[keyof typeof Direction];
+export type Direction = typeof Direction[keyof typeof Direction];
 
 export class Crossroad {
   constructor(
-    readonly current: Step,
-    readonly direction: Direction,
-    readonly choices: Record<Direction, Step>,
+    private readonly current: Step,
+    private readonly direction: Direction,
+    private readonly choices: Record<Direction, Step>,
   ) { }
 
   move() {
     switch (this.current.type) {
+      case StepType.START:
+        return this.start();
+
       case StepType.HORIZONTAL:
       case StepType.VERTICAL:
         return this.continue();
@@ -29,27 +32,31 @@ export class Crossroad {
     }
   }
 
-  continue() {
-    return this.choices[this.direction];
+  private start() {
+    const directions = Object.entries(this.choices)
+      .map(n => n[1].canStepOn ? n[0] as Direction : undefined)
+      .filter(Boolean);
+    if (directions.length === 1) return directions[0];
   }
 
-  turn() {
+  private continue() {
+    if (this.choices[this.direction].canStepOn) return this.direction;
+  }
+
+  private turn() {
     switch (this.direction) {
       case Direction.UP:
       case Direction.DOWN:
-        if (this.choices.LEFT.canStepOn && this.choices.RIGHT.canStepOn)
-          return undefined;
-        if (this.choices.LEFT.canStepOn) return this.choices.LEFT;
-        if (this.choices.RIGHT.canStepOn) return this.choices.RIGHT;
-        break;
-
+        if (this.choices.LEFT.canStepOn !== this.choices.RIGHT.canStepOn) {
+          if (this.choices.LEFT.canStepOn) return Direction.LEFT;
+          if (this.choices.RIGHT.canStepOn) return Direction.RIGHT;
+        }
       case Direction.LEFT:
       case Direction.RIGHT:
-        if (this.choices.UP.canStepOn && this.choices.DOWN.canStepOn)
-          return undefined;
-        if (this.choices.UP.canStepOn) return this.choices.UP;
-        if (this.choices.DOWN.canStepOn) return this.choices.DOWN;
-        break;
+        if (this.choices.UP.canStepOn !== this.choices.DOWN.canStepOn) {
+          if (this.choices.UP.canStepOn) return Direction.UP;
+          if (this.choices.DOWN.canStepOn) return Direction.DOWN;
+        }
     }
   }
 }
